8d5821502db1d81a7844c9c2fae4807ac42b273b
hsh30835 <login7h@gmail.com>
2023-12-05 22:20:19 +0900

    main

---
+++ chap02-handler-method/src/main/java/com/ohgiraffers/handlermethod/Chap02HandlerMethodApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.handlermethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap02HandlerMethodApplication { //얘가 SpringBoot를 실행시켜줌
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap02HandlerMethodApplication.class, args);
+    }
+
+}
